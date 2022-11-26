59a2729e2bc7636cf1a7d6a88f0cd7a90d611abc
Jonathan Duriez <duriezj@gmail.com>
2022-11-26 17:01:55 +0100

    feat: init project

---
+++ src/main/java/io/conduktor/demos/kafkabeginnerscourse/KafkaBeginnersCourseApplication.java
@@ -0,0 +1,13 @@
+package io.conduktor.demos.kafkabeginnerscourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaBeginnersCourseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaBeginnersCourseApplication.class, args);
+	}
+
+}
